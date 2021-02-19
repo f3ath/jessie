50f96a7b97811763ef6adbc996222408e1aa2775
The Конь <f3ath@users.noreply.github.com>
2021-02-18 22:38:43 -0800

    Expressions (#21)

--- lib/src/selector/sequence.dart
+++ lib/src/selector/sequence.dart
@@ -1,18 +1,18 @@
 import 'package:json_path/src/json_path_match.dart';
 import 'package:json_path/src/selector.dart';
 
-typedef Filter = Iterable<JsonPathMatch> Function(
-    Iterable<JsonPathMatch> matches);
-
 class Sequence implements Selector {
   Sequence(Iterable<Selector> selectors)
-      : _filter = selectors.fold<Filter>(
+      : _filter = selectors.fold<_Filter>(
             (_) => _,
             (filter, selector) => (matches) =>
                 filter(matches).map(selector.apply).expand((_) => _));
 
-  final Filter _filter;
+  final _Filter _filter;
 
   @override
   Iterable<JsonPathMatch> apply(JsonPathMatch match) => _filter([match]);
 }
+
+typedef _Filter = Iterable<JsonPathMatch> Function(
+    Iterable<JsonPathMatch> matches);
