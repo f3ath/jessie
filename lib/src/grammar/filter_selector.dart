a7c8e520cf272a6ac6a50bb827d672bc3325f9fa
f3ath <karapetov@gmail.com>
2023-03-18 09:54:06 -0700

    formatting

--- lib/src/grammar/filter_selector.dart
+++ lib/src/grammar/filter_selector.dart
@@ -1,6 +1,6 @@
 import 'package:json_path/src/expression/expression.dart';
-import 'package:json_path/src/grammar/selector.dart';
 import 'package:json_path/src/grammar/all_children.dart';
+import 'package:json_path/src/grammar/selector.dart';
 
 Selector filterSelector(Expression<bool> filter) =>
     (node) => allChildren(node).where(filter.apply);
--- lib/src/grammar/wildcard.dart
+++ lib/src/grammar/wildcard.dart
@@ -1,5 +1,5 @@
-import 'package:json_path/src/grammar/parser_ext.dart';
 import 'package:json_path/src/grammar/all_children.dart';
+import 'package:json_path/src/grammar/parser_ext.dart';
 import 'package:petitparser/petitparser.dart';
 
 final wildcard = char('*').value(allChildren);
